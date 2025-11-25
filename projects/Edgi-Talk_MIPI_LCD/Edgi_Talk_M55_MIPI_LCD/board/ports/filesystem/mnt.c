8a2a0915a2394d1c20c46520a752c934216e78e1
Rbb666 <zhangbingru@rt-thread.com>
2025-11-25 17:38:50 +0800

    替换所有的mnt.c

--- projects/Edgi-Talk_MIPI_LCD/Edgi_Talk_M55_MIPI_LCD/board/ports/filesystem/mnt.c
+++ projects/Edgi-Talk_MIPI_LCD/Edgi_Talk_M55_MIPI_LCD/board/ports/filesystem/mnt.c
@@ -5,18 +5,42 @@
 
 int mnt_init(void)
 {
-    rt_err_t ret;
+    rt_device_t device;
 
-    rt_thread_mdelay(200);
+    rt_thread_mdelay(500);
 
-    if (dfs_mount("sd0", "/", "elm", 0, 0) != 0)
+    /* 检测sd0设备是否存在 */
+    device = rt_device_find("sd0");
+    if (device == RT_NULL)
     {
-        rt_kprintf("Dir / mount failed!\n");
+        rt_kprintf("SD card device 'sd0' not found!\n");
         return -1;
     }
 
-    return 0;
+    /* 尝试挂载SD卡 */
+    if (dfs_mount("sd0", "/", "elm", 0, 0) == 0)
+    {
+        rt_kprintf("SD card mount to '/' success!\n");
+        return 0;
+    }
+
+    /* 挂载失败，尝试格式化 */
+    rt_kprintf("SD card mount failed, try to mkfs...\n");
+    if (dfs_mkfs("elm", "sd0") == 0)
+    {
+        rt_kprintf("SD card mkfs success!\n");
+
+        /* 格式化成功后再次尝试挂载 */
+        if (dfs_mount("sd0", "/", "elm", 0, 0) == 0)
+        {
+            rt_kprintf("SD card mount to '/' success!\n");
+            return 0;
+        }
+    }
+
+    rt_kprintf("SD card mount to '/' failed!\n");
+    return -1;
 }
-INIT_ENV_EXPORT(mnt_init);
+INIT_APP_EXPORT(mnt_init);
 
 #endif
